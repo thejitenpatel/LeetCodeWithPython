54e6e18e4d65cb262331e6dfc59feb0c34921d52
thejitenpatel <jiten.pateljp2001@gmail.com>
2024-06-20 21:44:31 +0530

    remove duplicates from an array

---
+++ Arrays/remove_duplicate.py
@@ -0,0 +1,16 @@
+from typing import List
+
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        duplicates = {}
+
+        for num in nums:
+            if num in duplicates:
+                return True
+            duplicates[num] = True
+
+        return False
+
+
+sol = Solution()
+print(sol.hasDuplicate([1, 2, 3]))
