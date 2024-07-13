89bff57b42153b6637a9f34510c5fa871d7778c0
thejitenpatel <jiten.pateljp2001@gmail.com>
2024-07-13 18:59:07 +0530

    [1133] [Easy] Largest Unique Number premium

--- DSACourse/Hashing/largest_unique_number.py
+++ DSACourse/Hashing/largest_unique_number.py
@@ -1,21 +1,28 @@
-from collections import defaultdict
+from collections import Counter
 from typing import List
 
 
 class Solution:
     def largestUniqueNumber(self, nums: List[int]) -> int:
-        counts = defaultdict(int)
+        cntr = Counter(nums)
+        biggest = -1
 
-        for num in nums:
-            counts[num] += 1
+        for num, freq in cntr.items():
+            if freq == 1:
+                biggest = max(biggest, num)
+        return biggest
+        # counts = defaultdict(int)
 
-        result = -1
+        # for num in nums:
+        #     counts[num] += 1
 
-        for num, cnt in counts.items():
-            if cnt == 1 and num > result:
-                result = num
+        # result = -1
 
-        return result
+        # for num, cnt in counts.items():
+        #     if cnt == 1 and num > result:
+        #         result = num
+
+        # return result
 
 
 sol = Solution()
