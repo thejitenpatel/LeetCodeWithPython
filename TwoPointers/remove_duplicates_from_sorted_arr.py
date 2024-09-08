bfb875d49ca22142ef3619d43cfc22ecdf2ee096
thejitenpatel <jiten.pateljp2001@gmail.com>
2024-09-08 14:34:56 +0530

    [26] [Easy]  Remove Duplicates from Sorted Array

---
+++ TwoPointers/remove_duplicates_from_sorted_arr.py
@@ -0,0 +1,16 @@
+from typing import List
+
+
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        j = 0
+        for i in range(1, len(nums)):
+            if nums[i] != nums[j]:
+                j += 1
+                nums[j] = nums[i]
+
+        return j + 1
+
+
+sol = Solution()
+print(sol.removeDuplicates([1, 1, 2]))
