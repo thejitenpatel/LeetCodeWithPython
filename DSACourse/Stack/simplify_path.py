a5aeffa80d5cc98c41373ab35765025778bb9552
thejitenpatel <jiten.pateljp2001@gmail.com>
2024-09-15 16:49:29 +0530

    [71] [Medium]  Simplify Path

---
+++ DSACourse/Stack/simplify_path.py
@@ -0,0 +1,17 @@
+class Solution:
+    def simplifyPath(self, path: str) -> str:
+        stack = []
+
+        for portion in path.split("/"):
+            if portion == "..":
+                if stack:
+                    stack.pop()
+            elif portion == "." or not portion:
+                continue
+            else:
+                stack.append(portion)
+        return "/" + "/".join(stack)
+
+
+sol = Solution()
+print(sol.simplifyPath(path="/home/user/Documents/../Pictures"))
